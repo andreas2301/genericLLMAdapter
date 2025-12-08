2afe8674d64afdc7a3ec7b0fa40d90fc056ee45d
Andreas G <agreger0@gmail.com>
2025-12-08 17:43:23 +0100

    Create GenericLLMAdapterApplication class

---
+++ src/main/java/de/angr/genericllmadapter/GenericLLMAdapterApplication.java
@@ -0,0 +1,12 @@
+package de.angr.genericllmadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericLLMAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericLLMAdapter.class, args);
+    }
+}
